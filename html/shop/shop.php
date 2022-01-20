ee40180419bd39fbeabad05b215f5e9ab29d74e4
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2022-01-20 13:55:35 +0000

    Update shop.php

--- html/shop/shop.php
+++ html/shop/shop.php
@@ -57,7 +57,7 @@ if ($numOfItems > 0) {
 	    $price = number_format($row["price"], 2);
 	    echo '	<div class="column">';
 	    echo '			<div class="card">';
-	    echo '				<img src="' . $row["image_url"] . '" style="width: 50%" "height: 50%". class = "center">';
+	    echo '				<img src="' . $row["image_url"] . '" style="width: 50>';
 	    echo '				<div class="container">';
 	    echo '					<h2 class="productTitle">' . $row["product_name"] . '</h2>';
 	    echo '					<p class="center">' . $currency . $price . '</p>';
